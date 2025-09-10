5668ca53cdac5c9f5dd8cd9042d243b3ea4a4322
Thanhloc913 <huynhthanhloc913@gmail.com>
2025-09-10 16:19:58 +0700

    feat: implement MoviesService with CRUD operations and genre relations

--- movie-service/src/modules/movies/movies.service.ts
+++ movie-service/src/modules/movies/movies.service.ts
@@ -1,4 +1,43 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Movie } from './movie.entity';
+import { Repository } from 'typeorm/repository/Repository.js';
+import { ILike, In } from 'typeorm';
+import { Genre } from '../genres/genre.entity';
 
 @Injectable()
-export class MoviesService {}
+export class MoviesService {
+  constructor(
+    @InjectRepository(Movie)
+    private readonly movieRepository: Repository<Movie>,
+    @InjectRepository(Genre)
+    private readonly genreRepository: Repository<Genre>,
+  ) {}
+
+  getAllMovies(): Promise<Movie[]> {
+    return this.movieRepository.find({
+      relations: ['genres'],
+    });
+  }
+
+  getMovieById(id: string): Promise<Movie | null> {
+    return this.movieRepository.findOne({
+      where: { id },
+      relations: ['genres'],
+    });
+  }
+
+  getMoviesByGenre(genreId: string): Promise<Movie[]> {
+    return this.movieRepository.find({
+      relations: ['genres'],
+      where: { genres: { id: genreId } },
+    });
+  }
+
+  searchMovies(keyword: string): Promise<Movie[]> {
+    return this.movieRepository.find({
+      where: { title: ILike(`%${keyword}%`) },
+      relations: ['genres'],
+    });
+  }
+}

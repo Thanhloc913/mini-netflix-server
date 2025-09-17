b3c05140719d7423f1605c50f890bc356d207a02
Thanhloc913 <huynhthanhloc913@gmail.com>
2025-09-17 19:12:00 +0700

    feat: update CORS configuration to allow multiple origins for client access

--- gateway/src/main.ts
+++ gateway/src/main.ts
@@ -4,7 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: [
+      'http://localhost:5173',
+      'https://mini-netflix-client-kboxu04nc-thanhlc8-681315ca.vercel.app',
+      'https://mini-netflix-client.vercel.app',
+    ],
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 3000);
